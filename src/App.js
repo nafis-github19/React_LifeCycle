5fcd83cf2932d3640baddeb469a442ec329e95d6
unknown <nafis.github@gmail.com>
2020-03-04 20:40:39 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class LifeCycle extends Component {
+  constructor(){
+    super();
+    this.state = {
 
-export default App;
+      items: [],
+      isLoaded: false,
+
+    };
+  };
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then( response =>{
+      return response.json();
+    })
+    .then( data => {
+      console.log(data);
+      this.setState({
+        items: data,
+        isLoaded: true,
+      })
+    })
+  };
+
+
+  render(){
+    var {isLoaded, items} = this.state;
+
+    if(!isLoaded){
+      return(
+        <div>Loading......</div>
+      )
+    }else{
+
+
+
+    return (
+
+      <div>
+        <ul>
+          {items.map(el =>{
+            return(
+              <li key={el.id}>
+              Name: {el.name} | UserName: {el.username} | {" "}
+              <a href={`https://${el.website}`}>Website</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+   };
+  };
+};
+
+export default LifeCycle;
